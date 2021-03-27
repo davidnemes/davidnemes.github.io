a22de702ac4ee680c86871fc573cf880db84c03f
davidnemes <64535713+davidnemes@users.noreply.github.com>
2021-03-27 13:56:39 +0100

    start working on checkGameEnd

--- five-in-a-rowScripts.js
+++ five-in-a-rowScripts.js
@@ -10,33 +10,109 @@ const game = Vue.createApp({
         }
     },
     methods: {
-        gotClick(e) {
-            let id = e.target.id
+        getIdNum(id, forArray) {
             let splittedId = id.split('-')
-            let numId = parseInt(splittedId[1]) - 1
+            if(forArray) {
+                return parseInt(splittedId[1]) - 1
+            }
+            return parseInt(splittedId[1])
+        },
+        gotClick(e) {
+            numId = this.getIdNum(e.target.id, true)
 
-            if (!this.gameList[numId].isLocked) {
+            if (!this.gameList[numId].state) {
                 if(this.xTurn) {
                     this.gameList[numId].src = this.xSrc
+                    this.gameList[numId].state = 'x'
                 } else {
                     this.gameList[numId].src = this.oSrc
+                    this.gameList[numId].state = 'o'
                 }
                 this.xTurn = !this.xTurn
-                this.gameList[numId].isLocked = true
             }
+        },
+        checkGameEnd() {
+            let sqsInARow = Math.sqrt(this.numOfSquares)
+            this.gameList.forEach(square => {
+                if(!square.state) {
+                    return
+                }
+                let sqId = this.getIdNum(square.id, false)
+                let sqPos = square.position
+                switch(sqPos) {
+                    case 'tlc':
+                        
+                    break
+                    case 'trc':
+
+                    break
+                    case 'blc':
+
+                    break
+                    case 'brc':
+
+                    break
+                    case 'top':
+
+                    break
+                    case 'bottom':
+
+                    break
+                    case 'left':
+
+                    break
+                    case 'right':
+
+                    break
+                    case 'normal':
+
+                    break
+                    default:
+                        alert('Oops.. something went really wrong!')
+                }
+
+            })
         },
         resetGame() {
             this.gameList.forEach(item => {
                 item.src = this.defaultSrc
-                item.isLocked = false
+                item.state = null
             })
             this.xTurn = true
         },
         //ez a method a játék előkészítését végzi, csak egyszer az elején van használva
         prepareGame() {
             for(let i=1; i<=this.numOfSquares; i++) {
+                
                 let id = `item-${i}`
-                this.gameList.push({id: id, isLocked: false, src: this.defaultSrc})
+                let sqsInARow = Math.sqrt(this.numOfSquares)
+                let sqPosition
+
+                if(i <= sqsInARow) {
+                    if(i === 1) {
+                        sqPosition = 'tlc' // aka top left corner
+                    } else if(i === sqsInARow) {
+                        sqPosition = 'trc' // aka top right corner
+                    } else {
+                        sqPosition = 'top'
+                    }
+                } else if(i >= this.numOfSquares - sqsInARow) {
+                    if(i === this.numOfSquares - sqsInARow) {
+                        sqPosition = 'blc' // aka bottom left corner
+                    } else if (i === this.numOfSquares) {
+                        sqPosition = 'brc' // aka bottom right corner
+                    } else {
+                        sqPosition = 'bottom'
+                    }
+                } else if(i % sqsInARow === 0){
+                    sqPosition = 'right'
+                } else if(i % sqsInARow === 1){
+                    sqPosition = 'left'
+                } else {
+                    sqPosition = 'normal'
+                }
+
+                this.gameList.push({id: id, state: null, position: sqPosition, src: this.defaultSrc})
             }
             //itt a gameBody szélességét számolom ki, állítom be a responsive megjelenéssel együtt
             let sqrt = Math.sqrt(this.numOfSquares)
@@ -47,6 +123,6 @@ const game = Vue.createApp({
             let r = document.querySelector(':root')
             r.style.setProperty('--gameBodyWidth', widthPx)
             r.style.setProperty('--gameBodyWidthOnMobile', widthPxMobile)
-        }
+        },
     }
 })
