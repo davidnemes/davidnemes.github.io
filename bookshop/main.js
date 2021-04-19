4ace379b6de7199d14be1abbf15d9e312180f122
davidnemes <64535713+davidnemes@users.noreply.github.com>
2021-04-19 20:15:41 +0200

    start working on a new project: bookshop

---
+++ bookshop/main.js
@@ -0,0 +1,13 @@
+const bookShop = Vue.createApp({
+    data() {
+        return {
+            data: 'data'
+        }
+    },
+    methods: {
+
+    },
+})
+
+// un = username ; pw = password
+let accounts = [{un: 'bigreader01', pw: 'povHey9'}, {un: "jonas74", pw: "aks5We"}, {un: "phil_13", pw: "7ebnu8"}]
